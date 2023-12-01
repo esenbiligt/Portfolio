2753696825b81074a76a96f0c13d63f78334f477
esenbiligt bat-ochir <52008y300@gmail.com>
2023-12-01 12:20:20 +0800

    test

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
-
+import { SVG } from '@/components/Svg'
 export default function Home() {
   return (
     <main>
       TEST!
+      <SVG></SVG>
     </main>
   )
 }
